995a631413070cfe20acb313857b57eca6d20031
Kareem Khaled <kareem.khaled1000@gmail.com>
2023-11-10 01:36:32 +0200

    Initial Commit

---
+++ AppetizersApp/AppetizersAppApp.swift
@@ -0,0 +1,17 @@
+//AppetizersApp
+ //AppetizersAppApp.swift
+
+
+ //Created by: Kareem on 11/10/23
+ //Copyright (c) 2023 Kareem
+
+import SwiftUI
+
+@main
+struct AppetizersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
